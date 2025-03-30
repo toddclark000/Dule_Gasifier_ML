d76d36dff5e6543de64f31d4d02c67b398d7b55b
Todd Clark <toddclark904@gmail.com>
2025-03-30 11:23:43 -0600

    added plot function to helper

--- helper_functions.py
+++ helper_functions.py
@@ -18,6 +18,34 @@ def to_datetime(df):
         df.loc[~mask_ampm, 'Date'] + ' ' + df.loc[~mask_ampm, 'Time'],
         format='%m/%d/%Y %H:%M:%S'
     )
+
+    df = df.drop(columns = ['Date', 'Time'])
     
     return df
 
+import matplotlib.pyplot as plt
+
+def plot_all_fields(df, title):
+    '''
+    This is an edited chat gpt function. Gpt 04 Prompt: "s there a good way to plot each field in a df against the time in a long subplot that has one column and lots of rows"
+    '''
+    time_column = 'Datetime'
+
+    num_fields = df.shape[1] - 1  # Exclude time column
+    fields = [col for col in df.columns if col != time_column]
+
+    fig, axes = plt.subplots(nrows=num_fields, ncols=1, figsize=(12, 0.5 * num_fields), sharex=True)
+
+    # If only one axis (just one field), make it iterable
+    if num_fields == 1:
+        axes = [axes]
+
+    for ax, field in zip(axes, fields):
+        ax.plot(df[time_column], df[field])
+        ax.set_ylabel(field)
+        ax.grid(True)
+
+    axes[-1].set_xlabel(time_column)
+    fig.tight_layout()
+    plt.title(title)
+    plt.show()
